41ea27bfeeb36283f8dcec8ba3e07700b1d4dde8
Muhammad Muzzamil Khan <khanmuhammadmuzzamil500@gmail.com>
2024-10-26 16:14:52 +0500

    OTP Verification page added under pages/general folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import FAQ from "./pages/general/FAQ";
 import RegisterUser from "./pages/general/RegisterUser";
 import Login from "./pages/general/Login";
 import ForgetPassword from "./pages/general/ForgetPassword";
+import OTPVerification from "./pages/general/OTPVerification";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
           <Route path="/register" element={<RegisterUser />} />
           <Route path="/login" element={<Login />} />
           <Route path="/forgetpassword" element={<ForgetPassword />} />
+          <Route path="/verify" element={<OTPVerification />} />
 
         </Routes>
     </>

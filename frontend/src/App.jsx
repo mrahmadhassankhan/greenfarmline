8b80dcf69ac310a8a6f53af292e086b256c3eae3
Ahmad Hassan Khan <codewithahk@gmail.com>
2024-12-25 22:40:14 +0500

    fix:fix the wrong path of the file UserQueries file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/general/Home";
 import Contact from "./pages/general/Contact";
 import About from "./pages/general/About";
@@ -15,43 +15,46 @@ import UserForumView from "./pages/user/discussionForum/UserForumView";
 import UserDashboard from "./pages/user/dashboard/UserDashboard";
 import PostQuery from "./pages/user/discussionForum/PostQuery";
 import QueryDetailedView from "./pages/user/discussionForum/QueryDetailedView";
-import UserQueries from "./pages/user/discussionForum/userqueries";
+import UserQueries from "./pages/user/discussionForum/UserQueries";
 import ExpertDashboard from "./pages/expert/dashboard/ExpertDashboard";
 import ExpertForumView from "./pages/expert/discussionforum/ExpertForumView";
 import ExpertAnswerView from "./pages/expert/discussionforum/ExpertAnswerView";
 import ExpertAnsweredQueries from "./pages/expert/discussionforum/ExpertAnsweredQueries";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   return (
     <>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/privacypolicy" element={<PrivacyPolicy />} />
-          <Route path="/terms" element={<Terms />} />
-          <Route path="/faq" element={<FAQ />} />
-          <Route path="/register" element={<RegisterUser />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/forgetpassword" element={<ForgetPassword />} />
-          <Route path="/verify" element={<OTPVerification />} />
-          <Route path="/discussionforum" element={<DiscussionForum />} />
+      <ToastContainer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/privacypolicy" element={<PrivacyPolicy />} />
+        <Route path="/terms" element={<Terms />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/register" element={<RegisterUser />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/verify" element={<OTPVerification />} />
+        <Route path="/discussionforum" element={<DiscussionForum />} />
 
-          <Route path="/username" element={<UserDashboard />} />
-          <Route path="/userforumview" element={<UserForumView />} />
-          <Route path="/post-query" element={<PostQuery />} />
-          <Route path="/query-detailed-view" element={<QueryDetailedView />} />
-          <Route path="/your-queries" element={<UserQueries />} />
+        <Route path="/username" element={<UserDashboard />} />
+        <Route path="/userforumview" element={<UserForumView />} />
+        <Route path="/post-query" element={<PostQuery />} />
+        <Route path="/query-detailed-view" element={<QueryDetailedView />} />
+        <Route path="/your-queries" element={<UserQueries />} />
 
-
-          <Route path="/expertname" element={<ExpertDashboard />} />
-          <Route path="/expertforumview" element={<ExpertForumView />} />
-          <Route path="/write-your-answer" element={<ExpertAnswerView />} />
-          <Route path="/your-answered-queries" element={<ExpertAnsweredQueries />} />
-
-        </Routes>
+        <Route path="/expertname" element={<ExpertDashboard />} />
+        <Route path="/expertforumview" element={<ExpertForumView />} />
+        <Route path="/write-your-answer" element={<ExpertAnswerView />} />
+        <Route
+          path="/your-answered-queries"
+          element={<ExpertAnsweredQueries />}
+        />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;

89cd4e26e03a5b0d1ac7b42836f88950a7d2d669
Ahmad Hassan Khan <codewithahk@gmail.com>
2024-12-29 03:11:03 +0500

    refactor:added semicolon

--- frontend/src/pages/admin/discussionforum/ApprovedQueries.jsx
+++ frontend/src/pages/admin/discussionforum/ApprovedQueries.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import AdminNav from '../AdminNav'
+import React from "react";
+import AdminNav from "../AdminNav";
 
 function ApprovedQueries() {
   return (
     <>
-        <AdminNav />
+      <AdminNav />
     </>
-  )
+  );
 }
 
-export default ApprovedQueries
+export default ApprovedQueries;

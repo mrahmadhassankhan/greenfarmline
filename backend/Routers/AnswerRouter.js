ab3d0c5861fe5183c0af81f085a666e1525e4239
Ahmad Hassan Khan <codewithahk@gmail.com>
2024-12-30 00:19:13 +0500

    feat:added the answer router and defined the routes

---
+++ backend/Routers/AnswerRouter.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const answerrouter = express.Router();
+const answerController = require("../Controllers/Answer/AnswerController");
+
+// Route to add vote to an answer
+answerrouter.post("/addVote", answerController.addVoteToAnswer);
+
+// Route to downvote an answer
+answerrouter.post("/downvote", answerController.downvoteAnswer);
+
+// Route to get answers for a specific query
+answerrouter.get("/answers/:queryId", answerController.getAnswersByQueryId);
+
+// Route to post an answer to a query
+answerrouter.post("/postAnswer", answerController.postAnswerToQuery);
+
+//Total no of votes of specific query
+answerrouter.get("/votes/:answerId", answerController.getVoteCount);
+
+// Route to get all answers by a specific expert user
+answerrouter.get(
+  "/expert/:userEmail",
+  answerController.getQueriesByExpertEmail
+);
+
+module.exports = answerrouter;

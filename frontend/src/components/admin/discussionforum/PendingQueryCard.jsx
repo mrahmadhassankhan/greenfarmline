b4aff159e2689205d38dbd1e76ca202fa3047085
Muhammad Muzzamil Khan <khanmuhammadmuzzamil500@gmail.com>
2024-12-29 16:06:41 +0500

    fix: adjust the query title width to prevent the overflow from the card view and align the action buttons vertically in card view.

--- frontend/src/components/admin/discussionforum/PendingQueryCard.jsx
+++ frontend/src/components/admin/discussionforum/PendingQueryCard.jsx
@@ -26,7 +26,7 @@ const PendingQueryCard = ({
 
     return (
         <div
-            className="relative flex flex-col md:flex-row items-start bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow"
+            className="relative flex flex-col md:flex-row md:gap-5 items-start bg-white shadow-lg rounded-lg p-4 hover:shadow-xl transition-shadow"
         >
             {image && (
                 <img onClick={onClick}
@@ -35,19 +35,15 @@ const PendingQueryCard = ({
                     className="w-full md:w-1/3 h-40 object-cover rounded-lg md:mr-4 cursor-pointer"
                 />
             )}
-            <div className="flex-1 flex flex-col h-full">
-                <div>
-                    <h3 onClick={onClick} className="text-xl font-bold text-lime-600 cursor-pointer">{title}</h3>
-                    <p className="text-sm text-gray-600 my-2 overflow-hidden break-words max-h-20 max-w-96">
+            <div className="flex-1 w-full md:w-1/2">
+                    <h3 onClick={onClick} className="text-xl font-bold text-lime-600 cursor-pointer overflow-hidden break-words max-h-8">{title}</h3>
+                    <p className="text-sm text-gray-600 my-2 overflow-hidden break-words max-h-20 max-w-full">
                         {description.length > 120 ? `${description.slice(0, 120)}...` : description}
                     </p>
-                </div>
-                <div className='absolute bottom-4'>
-                    <div className="flex gap-20 text-gray-500 text-sm">
+                    <div className="absolute bottom-4 flex gap-20 text-gray-500 text-sm">
                         <span>Posted by: {author}</span>
                         <span>Date: {date}</span>
                     </div>
-                </div>
             </div>
 
             {/* Badge */}
@@ -59,7 +55,7 @@ const PendingQueryCard = ({
 
             {/* Action Buttons */}
             {status === "Pending" && (
-                <div className="mt-4 flex space-x-2">
+                <div className="mt-4 flex flex-col space-y-2">
                     <button
                         onClick={onApprove}
                         className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 transition"

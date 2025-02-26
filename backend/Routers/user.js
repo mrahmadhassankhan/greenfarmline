638c18dd19874b026f3ce8110e9a12311ac809d9
Ahmad Hassan Khan <codewithahk@gmail.com>
2025-02-27 04:35:10 +0500

    fix:removed the whole wrapping of authmiddleware which was causing the issue  excluded login and register route issue fixed

--- backend/Routers/user.js
+++ backend/Routers/user.js
@@ -12,14 +12,15 @@ const {
 } = require("../Controllers/user");
 const roleMiddleware = require("../middlewares/role");
 const authMiddleware = require("../middlewares/auth");
-router.use(authMiddleware);
 router.route("/register").post(UploadingFileMiddleware, register);
 router.route("/login").post(login);
-router.route("/verify").get(roleMiddleware(["farmer"]), verifyUser);
-router.route("/orders").get(roleMiddleware(["farmer"]), getOrder);
 router
-  .route("/forgetpassword/:email")
-  .get(roleMiddleware(["farmer", "seller", "expert"]), forgetPassword);
+  .route("/verify")
+  .get(authMiddleware, roleMiddleware(["farmer"]), verifyUser);
+router
+  .route("/orders")
+  .get(authMiddleware, roleMiddleware(["farmer"]), getOrder);
+router.route("/forgetpassword/:email").get(forgetPassword);
 router.route("/profile").get(authMiddleware, getUserProfile);
 router.route("/profile/update").put(authMiddleware, updateUserProfile);
 

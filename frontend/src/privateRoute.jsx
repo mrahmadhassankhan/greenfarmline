0e326760921fd2a11c747cd211f9717b4617501f
Ahmad Hassan Khan <codewithahk@gmail.com>
2025-02-27 03:32:05 +0500

    fix:inefficency in bundling due to static import

--- frontend/src/privateRoute.jsx
+++ frontend/src/privateRoute.jsx
@@ -1,14 +1,24 @@
-import { useContext } from "react";
+import { useContext, Suspense, lazy } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "../authContext/auth";
-import ErrorPage from "./pages/sellerpages/pages/ErrorPage";
+import TriangleLoader from "./components/seller/TriangleLoader";
+
+const ErrorPage = lazy(() =>
+  import("../src/pages/sellerpages/pages/ErrorPage")
+);
 
 const PrivateRoute = () => {
   const { user, loading } = useContext(AuthContext);
 
   if (loading) return <p>Loading...</p>;
 
-  return user ? <Outlet /> : <ErrorPage />;
+  return user ? (
+    <Outlet />
+  ) : (
+    <Suspense fallback={<TriangleLoader />}>
+      <ErrorPage />
+    </Suspense>
+  );
 };
 
 export default PrivateRoute;

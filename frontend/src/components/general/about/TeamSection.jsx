93c6cd3c353c313c42e1d67ed76cb772561555d3
Ahmad Hassan Khan <codewithahk@gmail.com>
2025-03-03 21:50:24 +0500

    feat:added links in team section

--- frontend/src/components/general/about/TeamSection.jsx
+++ frontend/src/components/general/about/TeamSection.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import MuzzamilKhanImage from "../../../images/team/Muzzamil Khan.png";
 import HassanKhanImage from "../../../images/team/Hassan Khan.png";
+import { FaLink } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const teamMembers = [
   { name: "Muzzamil Khan", title: "CEO", image: MuzzamilKhanImage },
-  { name: "Hassan Khan", title: "CTO", image: HassanKhanImage },
+  { name: "Ahmad Hassan Khan", title: "CTO", image: HassanKhanImage },
+];
+const links = [
+  { url: "https://linktr.ee/" },
+  { url: "https://linktr.ee/mrahmadhassankhan" },
 ];
 
 const TeamSection = () => {
@@ -15,15 +21,26 @@ const TeamSection = () => {
         </h2>
         <div className="flex flex-col md:flex-row items-center justify-center">
           {teamMembers.map((member, idx) => (
-            <div key={idx} className="w-full md:w-1/3 px-4 py-4">
+            <div
+              key={idx}
+              className="w-full md:w-1/3 px-4 py-4 flex  flex-col content-center items-center"
+            >
               <img
                 src={member.image}
                 alt={member.name}
                 className="w-48 h-48 rounded-full object-cover mx-auto shadow-2xl bg-green-600"
               />
-              <h3 className="text-2xl font-semibold text-green-600 dark:text-green-300 mt-4">
+
+              <h3 className="text-2xl font-semibold text-green-600 dark:text-green-300 mt-4 flex content-center items-center">
+                <Link to={links[idx].url}>
+                  <FaLink
+                    size="1rem"
+                    className="text-green-600 mr-1 hover:text-green-200"
+                  />
+                </Link>
                 {member.name}
               </h3>
+
               {/* <p className="text-gray-600 dark:text-gray-300">{member.title}</p> */}
             </div>
           ))}
